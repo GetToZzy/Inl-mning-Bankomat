d22ab24ebb7ba4d128210bae7e42c27fa9c12c6a
Max Jokela <maxjokelas@gmail.com>
2022-09-30 16:37:01 +0200

    Hej

---
+++ bankomat.py
@@ -0,0 +1,8 @@
+print("**Sparbanken**")
+print("1. Skapa konto")
+print("2. Logga in")
+print("3. Avsluta")
+p=int(input("Gör ett val: "))
+if(p==1):
+    print("Skriv ett kontonummer: ")
+    input("")
